8feb18d8581cb4e6e55531594fc6ab11a7281bca
Matt <matt@crazy-logic.co.uk>
2018-09-25 13:03:44 +0100

    Create Uinstall.sh

---
+++ Uinstall.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Matts installer... 
+
+sudo apt-get-update 
+
+sudo apt-get install net-tools git openssh-server simplescreenrecorder vlc 
+
+
+
+
+sudo apt-get install build-essential cmake pkg-config libboost-all-dev libssl-dev libunbound-dev \
+libminiupnpc-dev libunwind8-dev liblzma-dev libldns-dev libexpat1-dev
