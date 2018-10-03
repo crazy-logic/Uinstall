2b16e73dad3b5b6038c630cc1d6a6a68343bb261
Matt <matt@crazy-logic.co.uk>
2018-10-03 01:10:33 +0100

    Update Uinstall.sh

--- Uinstall.sh
+++ Uinstall.sh
@@ -11,3 +11,15 @@ sudo apt-get install net-tools git openssh-server simplescreenrecorder vlc
 
 sudo apt-get install build-essential cmake pkg-config libboost-all-dev libssl-dev libunbound-dev \
 libminiupnpc-dev libunwind8-dev liblzma-dev libldns-dev libexpat1-dev
+
+
+
+
+
+sudo add-apt-repository ppa:graphics-drivers/ppa
+
+sudo apt update
+
+sudo ubuntu-drivers autoinstall
+
+sudo apt install nvidia-cuda-toolkit gcc-6
