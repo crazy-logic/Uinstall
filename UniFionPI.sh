dded7746bf6f1ae59bcbabf2a23b2eb59683380d
Matt <matt@crazy-logic.co.uk>
2018-12-19 11:41:28 +0000

    Update UniFionPI.sh

--- UniFionPI.sh
+++ UniFionPI.sh
@@ -2,17 +2,17 @@
 
 echo lets install unifi. 
 
-// need to do updates first 
+# need to do updates first 
 sudo apt-get update 
 
-// then some dependancies 
+# then some dependancies 
 sudo apt-get install oracle-java8-jdk
 sudo apt-get install dirmngr
 
-// now lets add ubiquity's package server and update again. 
+# now lets add ubiquitys package server and update again. 
 echo 'deb http://www.ubnt.com/downloads/unifi/debian stable ubiquiti' | sudo te$
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv C0A52C50
 sudo apt-key update 
 
-// and now lets install 
+# and now lets install 
 sudo apt-get install unifi -y 
