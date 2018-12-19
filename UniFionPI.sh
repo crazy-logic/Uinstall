008cb4b7d0b554a22fb5f3f3a5a50de773d90e63
Matt <matt@crazy-logic.co.uk>
2018-12-19 11:50:15 +0000

    Update UniFionPI.sh

--- UniFionPI.sh
+++ UniFionPI.sh
@@ -13,6 +13,7 @@ sudo apt-get install dirmngr
 echo 'deb http://www.ubnt.com/downloads/unifi/debian stable ubiquiti' | sudo tee /etc/apt/sources.list.d/unifi.list
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv C0A52C50
 sudo apt-key update 
+sudo apt-get update 
 
 # and now lets install 
 sudo apt-get install unifi -y 
