64dfd8f97a347ea561004297cf9967efab020de4
Matt <matt@crazy-logic.co.uk>
2018-12-19 11:44:07 +0000

    Update UniFionPI.sh

--- UniFionPI.sh
+++ UniFionPI.sh
@@ -10,7 +10,7 @@ sudo apt-get install oracle-java8-jdk
 sudo apt-get install dirmngr
 
 # now lets add ubiquitys package server and update again. 
-echo 'deb http://www.ubnt.com/downloads/unifi/debian stable ubiquiti' | sudo te$
+echo 'deb http://www.ubnt.com/downloads/unifi/debian stable ubiquiti' | sudo tee /etc/apt/sources.list.d/unifi.list
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv C0A52C50
 sudo apt-key update 
 
