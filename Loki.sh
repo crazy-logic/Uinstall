c70e7f3de6b41630e6d06a76b509f4b1ff4dfb60
Matt <matt@crazy-logic.co.uk>
2018-12-22 16:32:42 +0000

    Create Loki.sh

---
+++ Loki.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo lets build Loki
+
+sudo apt install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev -y 
+sudo apt-get install git -y
+sudo apt-get install build-essential cmake pkg-config libboost-all-dev libssl-dev libzmq3-dev	libpgm-dev libunbound-dev libsodium-dev
+
+cd /
+mkdir crypto/loki 
+cd crypro/loki
+git clone --recursive https://github.com/loki-project/loki
+mkdir /build
+make 
