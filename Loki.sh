358166e1969fb515225dee6d96774a01d120ce85
Matt <matt@crazy-logic.co.uk>
2018-12-22 16:40:39 +0000

    Update Loki.sh

--- Loki.sh
+++ Loki.sh
@@ -4,7 +4,7 @@ echo lets build Loki
 
 sudo apt install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev -y 
 sudo apt-get install git -y
-sudo apt-get install build-essential cmake pkg-config libboost-all-dev libssl-dev libzmq3-dev	libpgm-dev libunbound-dev libsodium-dev
+sudo apt-get install build-essential cmake pkg-config libboost-all-dev libssl-dev libzmq3-dev	libpgm-dev libunbound-dev libsodium-dev -y
 
 cd /
 mkdir crypto/loki 
